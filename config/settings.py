a3ea20f108f25bcd4e0077c8b8f85fe02e1c5afd
uttam-sin <uttam.singh@bobble.ai>
2025-09-07 12:44:09 +0530

    setting updated

--- config/settings.py
+++ config/settings.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Settings:
     BASE_URL = os.getenv("BASE_URL", "https://test.wing.work")
-    TEST_USER = os.getenv("TEST_USER", "testsuttam@gmail.com")
-    TEST_PASS = os.getenv("TEST_PASS", "Aa@123456789")
+    TEST_USER = os.getenv("TEST_USER", "TEST_USER")
+    TEST_PASS = os.getenv("TEST_PASS", "TEST_PASS")
     API_BASE = os.getenv("API_BASE")
     API_TOKEN = os.getenv("API_TOKEN")
     HEADLESS = os.getenv("HEADLESS", "false").lower() in ("1", "true", "yes")
